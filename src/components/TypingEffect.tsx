0b9450512406dcb140895af5f1db2c7eada0410f
jxlee <jimmyfalcon007@gmail.com>
2025-01-20 13:42:15 +0530

    Update framer-motion to version 11.18.1, replace Typical with TypeAnimation in TypingEffect component, and adjust Vite config for external dependencies

--- src/components/TypingEffect.tsx
+++ src/components/TypingEffect.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import Typical from 'react-typical';
+import { TypeAnimation } from 'react-type-animation';
 
 const TypingEffect = () => {
-    return (
-        <h3 className=" inline-block text-lg lg:text-xl text-teal-600">
-            <Typical
-                steps={[
-                    'Technical Business Analyst', 2000,
-                    'Scrum Master', 2000,
-                    'Technical Product Manager', 2000,
-                ]}
-                loop={1}
-                wrapper="span"
-            />
-        </h3>
-    );
+  return (
+    <h3 className="inline-block text-lg lg:text-xl text-teal-600">
+      <TypeAnimation
+        sequence={[
+          'Technical Business Analyst', // Type 'Technical Business Analyst'
+          2000, // Wait for 2 seconds
+          'Scrum Master', // Type 'Scrum Master'
+          2000, // Wait for 2 seconds
+          'Technical Product Manager', // Type 'Technical Product Manager'
+          2000, // Wait for 2 seconds
+          () => {
+            console.log('Sequence completed'); // Callback when the animation is completed
+          }
+        ]}
+        wrapper="span" // Wrap the animated text in a <span> tag
+        cursor={true} // Show the cursor
+        repeat={Infinity} // Repeat the sequence infinitely
+        style={{ fontSize: '2em', display: 'inline-block' }} // Custom style for the text
+      />
+    </h3>
+  );
 };
 
 export default TypingEffect;
